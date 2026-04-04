26ee3a857dc4de885b265d80468a18cd3ccb0eeb
Alexey4717 <lehabourne@gmail.com>
2026-04-04 22:25:51 +1000

    chore: update page widget

--- src/widgets/Page/ui/Page.tsx
+++ src/widgets/Page/ui/Page.tsx
@@ -1,33 +1,43 @@
+import { type ReactNode } from 'react';
+
+import { cn } from '@/shared/lib/styles/cn';
 import { Typography } from '@/shared/ui/Typography/Typography';
 
-import { ScrollPositionSetter } from './components/ScrollPositionSetter';
+import { PageRoot } from './components/PageRoot';
 import { ScrollPositionSetterProps } from './components/types';
 
 export interface PageProps extends ScrollPositionSetterProps {
-  heading?: string;
+  heading?: ReactNode;
+  withSavingScrollPosition?: boolean;
+  narrow?: boolean;
 }
 
 export const Page = ({
   children,
   heading,
+  withSavingScrollPosition = false,
+  narrow = false,
   className,
   'data-testid': dataTestId,
   mainRef,
   onScrollEnd,
 }: PageProps) => {
   return (
-    <ScrollPositionSetter
-      className={className}
+    <PageRoot
+      withSavingScrollPosition={withSavingScrollPosition}
+      className={cn(narrow && 'w-full lg:mx-auto lg:max-w-6xl', className)}
       data-testid={dataTestId}
       mainRef={mainRef}
       onScrollEnd={onScrollEnd}
     >
-      {heading && (
+      {typeof heading === 'string' ? (
         <Typography variant="h1" className="text-left">
           {heading}
         </Typography>
+      ) : (
+        (heading ?? null)
       )}
       {children}
-    </ScrollPositionSetter>
+    </PageRoot>
   );
 };
---
+++ src/widgets/Page/ui/components/PageRoot.tsx
@@ -0,0 +1,29 @@
+import { ScrollPositionSetter } from './ScrollPositionSetter';
+import { ScrollPositionSetterProps } from './types';
+
+const RootMain = (props: ScrollPositionSetterProps) => {
+  const { mainRef, className, children } = props;
+  return (
+    <main
+      ref={mainRef}
+      className={className}
+      data-testid={props['data-testid'] ?? 'Page'}
+    >
+      {children}
+    </main>
+  );
+};
+
+interface PageRootProps extends ScrollPositionSetterProps {
+  withSavingScrollPosition?: boolean;
+}
+
+export const PageRoot = (props: PageRootProps) => {
+  const { withSavingScrollPosition } = props;
+
+  if (withSavingScrollPosition) {
+    return <ScrollPositionSetter {...props} />;
+  }
+
+  return <RootMain {...props} />;
+};
